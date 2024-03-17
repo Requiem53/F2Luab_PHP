8814a8af685a8d602ba7a7bd2d0ae81344a5d3fd
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2024-03-17 11:57:09 +0800

    fixed tailwind issue

--- index.php
+++ index.php
@@ -12,10 +12,3 @@
     <p><a href="register.php">Register</a></p>
 </body>
 </html>
-
-
-<!-- <input type="text" placeholder="First Name" class="placeholder-white appearance-none bg-transparent border-b-2 w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none">
-<input type="text" placeholder="Last Name" class="placeholder-white appearance-none bg-transparent border-b-2 w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none">
-<input type="text" placeholder="Email" class="placeholder-white appearance-none bg-transparent border-b-2 w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none">
-<input type="text" placeholder="Password" class="placeholder-white appearance-none bg-transparent border-b-2 w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none">
-<input type="text" placeholder="Confirm Password" class="placeholder-white appearance-none bg-transparent border-b-2 w-full text-white mr-3 py-1 px-2 leading-tight focus:outline-none"> -->    
--- register.php
+++ register.php
@@ -6,16 +6,16 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=\, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="./output.css" rel="stylesheet">
+    <link href="./register.css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <title>4_LabExercise</title>
 </head>
 <body class="bg-registerPageBackground"> 
-    <form method="post" class="font-poppins text-2xl flex flex-col items-center justify-center my-10 gap-7 mx-auto w-3/12 p-14 bg-neutral-50 bg-opacity-15">   
+    <form method="post" class="font-poppins text-2xl flex flex-col items-center justify-center gap-7 my-6 mx-auto w-3/12 p-14 bg-neutral-50 bg-opacity-15">   
         <div class="mb-10 text-4xl text-white font-bold">Create your account</div>
             <div class="relative z-0 w-full">
                 <input type="text" name="regFirstName" class="block py-2.5 px-0 w-full text-xl text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-white dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
@@ -43,13 +43,13 @@
                     Username</label>
             </div>
             <div class="relative z-0 w-full">
-                <input type="text" name="regPassword" class="block py-2.5 px-0 w-full text-xl text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-white dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
+                <input type="password" name="regPassword" class="block py-2.5 px-0 w-full text-xl text-white bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-white dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " />
                 <label for="floating_standard" class="w-full absolute text-lg text-gray-200 dark:text-gray-200 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto">
                     Password</label>
             </div>
 
             <button type="submit" name="btnRegister" class="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded-full w-full mt-7">Sign Up</button>  
-            <div class="text-white text-sm">Already have an account? <a href="..\Login Page\loginPage.html"><span class="underline">Login</span></a></div>
+            <div class="text-white text-sm">Already have an account? <a href="login.php"><span class="underline">Login</span></a></div>
     </form>
 
     </div>
