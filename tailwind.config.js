3bb2d37e6598fc7f904248ed91725b7f6c87fbb3
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2024-04-20 13:33:03 +0800

    dark mode default maybe

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: 'media',
   content: ['./*{html,js,php}',
             './css/*{html,js,php}'
            ],
