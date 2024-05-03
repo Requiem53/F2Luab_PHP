88190c3cfe0151e3bf5eaf12b2bdf970695e6dde
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2024-05-03 17:51:57 +0800

    yeeeeyyy thank you maurice

---
+++ userProfile.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" class="dark">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="js\jquery-3.7.1.js"></script>
+    <link href="./css/userProfile.css" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body class="bg-registerPageBackground flex flex-col justify-center items-center my-12 mx-auto w-4/12">
+    <div class = "h-96 min-w-[65rem] w-[80%] bg-gray-900 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10" >
+        <img src="images/profilePics/defaultImage.jpg" class="object-scale-down h-52 w-52 my-9" style="border-radius: 50%;">
+    </div>
+</body>
+</html>
+<!-- flex flex-1 flex-col justify-center items-center my-12 mx-48 -->
+<!-- bg-registerPageBackground flex flex-col justify-center items-center my-12 mx-64 -->
