8c114f5fa7335d6a0c4201e0ecc4f56ad6da2609
Slamm Khelmer A. Luab <slammkhelmer@gmail.com>
2024-05-03 22:17:01 +0800

    added user profile

--- userProfile.php
+++ userProfile.php
@@ -5,13 +5,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="js\jquery-3.7.1.js"></script>
     <link href="./css/userProfile.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
-<body class="bg-registerPageBackground flex flex-col justify-center items-center my-12 mx-auto w-4/12">
-    <div class = "h-96 min-w-[65rem] w-[80%] bg-gray-900 rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10" >
-        <img src="images/profilePics/defaultImage.jpg" class="object-scale-down h-52 w-52 my-9" style="border-radius: 50%;">
+<body class="bg-[url('../images/backgroundPfps/defaultBackground.jpg')] bg-cover flex flex-col justify-center items-center my-12 mx-auto h-full min-w-[80rem] w-[80%] gap-10">
+    <div class = "h-64 min-w-[60rem] bg-gray-200 shadow-2xl rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10" >
+        <div class="flex flex-row items-center h-full ml-12">
+            <img src="images/profilePics/defaultImage.jpg" class="object-scale-down h-52 w-52" style="border-radius: 50%;">
+            <div class="flex flex-col ml-24 gap-10 font-poppins text-gray-50">
+                <p class="text-6xl ">Requiem53</p>
+                <p class="text-xl">Dedededededescription</p>
+            </div>
+        </div>
     </div>
+
+    <div class="min-h-[40rem] h-fit flex flex-row min-w-[60rem] gap-4 items-start justify-start">
+        <div class = "min-h-[40rem] h-full min-w-[63.5%] p-4 gap-2 flex flex-col justify-start items-center bg-gray-200 shadow-2xl rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10">
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+        </div>
+        <div class = "min-h-[40rem] h-full min-w-[35%] p-4 gap-2 flex flex-col justify-start items-center bg-gray-200 shadow-2xl rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10">
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>
+            <div class = "min-h-[6rem] min-w-[98%] bg-gray-200 shadow-lg rounded-xl bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10"></div>  
+        </div>
+    </div>
+
 </body>
 </html>
-<!-- flex flex-1 flex-col justify-center items-center my-12 mx-48 -->
-<!-- bg-registerPageBackground flex flex-col justify-center items-center my-12 mx-64 -->
